8dd76bfca1e959f45d1fe8bdb826bde05f81dfd2
vasilyev04 <146483091+vasilyev04@users.noreply.github.com>
2023-11-02 22:03:27 +0600

    Update Constants.java

--- app/src/main/java/com/kiparisov/cotrucking/model/Constants.java
+++ app/src/main/java/com/kiparisov/cotrucking/model/Constants.java
@@ -1,7 +1,7 @@
 package com.kiparisov.cotrucking.model;
 
 public class Constants {
-    public static final String FB_URL_CONNECTION = "https://co-trucking-f8527-default-rtdb.europe-west1.firebasedatabase.app/";
+    public static final String FB_URL_CONNECTION = "https://co-trucking-SECRET-default-rtdb.europe-west1.firebasedatabase.app/";
     public static final String SP_USER_SIGN_KEY = "Sign";
     public static final String FB_USERS_REFERENCE_KEY = "Users";
     public static final String FB_ADS_REFERENCE_KEY = "Ads";
@@ -12,6 +12,6 @@ public class Constants {
     public static final String FB_STORAGE_PROFILE_REFERENCE_KEY = "profileImages";
     public static final String FB_STORAGE_ADS_REFERENCE_KEY = "adImages";
     public static final String INTENT_EXTRA_AD_KEY = "AD_KEY";
-    public static final String FB_COTRUCKING_ACC_KEY = "-N4gIp0lf13ukUazL7bc";
+    public static final String FB_COTRUCKING_ACC_KEY = "SECRET";
     public static final String MY_TELEGRAM_LINK = "https://t.me/kiparisovr";
 }
